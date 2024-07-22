11edeb285fceff7a7a17dfac8c730215fa54aa59
MohamedFazil89 <nmohammedfazil790@gmail.com>
2024-07-22 21:18:23 +0530

    new

--- src/components/RightElement.jsx
+++ src/components/RightElement.jsx
@@ -3,14 +3,16 @@ import "./styles/right.css";
 import NavBar from "./NavBar";
 
 export default function Right(props) {
-    const [value, setValue] = useState([]); 
-    const [selectedValue, setSelectedValue] = useState(""); 
+    const [value, setValue] = useState([]);
+    const [segmentName, setSegmentName] = useState(""); 
+    const [selectedValue, setSelectedValue] = useState("");
 
     const handleSelectChange = (e) => {
         setSelectedValue(e.target.value);
     };
 
-    const handleAddSegment = () => {
+    const handleAddSegment = (e) => {
+        e.preventDefault();
         if (selectedValue && selectedValue !== "Add-schema") {
             setValue([...value, selectedValue]);
         }
@@ -19,30 +21,33 @@ export default function Right(props) {
     const handleRemoveSegment = (indexToRemove) => {
         setValue(value.filter((_, index) => index !== indexToRemove));
     };
+
     return (
-        <div className="Right-elements" style={{ width: `${props.widths}%` }}>
+        <div className="right-elements" style={{ width: `${props.widths}%` }}>
             <NavBar Title="Save Segment" />
-            <div className="Right-content">
+            <div className="right-content">
                 <h2>Enter the Name of the Segment</h2>
-                <input type="text" placeholder="Name of the Segment" name="segment" />
+                <input 
+                    type="text" 
+                    placeholder="Name of the Segment" 
+                    name="segment" 
+                    value={segmentName} 
+                    onChange={(e) => setSegmentName(e.target.value)} 
+                />
                 <p>To save your segment, you need to add the schemas to build the query</p>
                 <ul>
                     {value.map((item, index) => (
-                        <div className="loop-view">
-                            <select className="Datas" key={index}>
-                            <option value={item}>{item}</option>
-                            <option value={item}>{item}</option>
-                            <option value={item}>{item}</option>
-
+                        <div className="loop-view" key={`${item}-${index}`}>
+                            <select className={`Datas${index}`}>
+                                <option value={item}>{item}</option>
                             </select>
-                            <svg onClick={() => handleRemoveSegment(index) } xmlns="http://www.w3.org/2000/svg" height="50px" viewBox="0 -960 960 960" width="50px" fill="#5f6368"><path d="M200-440v-80h560v80H200Z" /></svg>
-
-                            </div>
-                            
-                            
+                            <svg onClick={() => handleRemoveSegment(index)} xmlns="http://www.w3.org/2000/svg" height="50px" viewBox="0 -960 960 960" width="50px" fill="#5f6368">
+                                <path d="M200-440v-80h560v80H200Z" />
+                            </svg>
+                        </div>
                     ))}
                 </ul>
-                <section className="option-container">
+                <div className="option-container">
                     <span className="dot"></span>
                     <select name="Add-schema" id="Add" onChange={handleSelectChange}>
                         <option value="Add-schema">Add Schema</option>
@@ -53,12 +58,11 @@ export default function Right(props) {
                         <option value="Account Name">Account Name</option>
                         <option value="City">City</option>
                     </select>
-                    <svg xmlns="http://www.w3.org/2000/svg" height="50px" viewBox="0 -960 960 960" width="50px" fill="#5f6368"><path d="M200-440v-80h560v80H200Z" /></svg>
-                </section>
-                <div>
-                    <a href="#" onClick={handleAddSegment}>+Add segment</a>
                 </div>
-                <div className="Save-div">
+                <div>
+                    <button onClick={handleAddSegment} className="add-segment">+ Add Segment</button>
+                </div>
+                <div className="save-div">
                     <button>Save the Segment</button>
                     <button>Cancel</button>
                 </div>
